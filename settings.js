82ff52b96df56ddbb88f452754bc0aeb2f10e935
pedropablo <pedropabloalbabrenes@gmail.com>
2018-09-24 17:24:24 +0200

    1

---
+++ settings.js
@@ -0,0 +1,8 @@
+adminAuth: {
+    type: "credentials",
+    users: [{
+        username: "admin",
+        password: "$2a$08$KnpRw2g.ToPSUO2AjTsxRudqtHQySU32fC2kNb9KFO1RWjNpMXSri",
+        permissions: "*"
+    }]
+}
